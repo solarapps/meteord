16f6be32f5296eb32089c907a9982bd40af888c7
Robin Van Cauter <robin@moonkin.co>
2017-12-05 16:01:16 +0100

    fix cibuild

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,11 +3,13 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-base" ../base && \
+#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-base" moonkin/meteord-ci:base
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-onbuild" ../onbuild && \
+#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-onbuild" moonkin/meteord-ci:onbuild
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-devbuild" ../devbuild && \
+#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-devbuild" moonkin/meteord-ci:devbuild
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-binbuild" ../binbuild && \
+#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-binbuild" moonkin/meteord-ci:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-cibuild" ../cibuild && \
+  docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-cibuild" moonkin/meteord-ci:cibuild
