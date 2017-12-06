95b57156ff3041e4aa44aed48ba909e7397203be
Robin Van Cauter <robin@moonkin.co>
2017-12-06 09:51:41 +0100

    add chrome driver

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -11,5 +11,5 @@ set -x
 #   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-devbuild" moonkin/meteord-ci:devbuild
 # docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-binbuild" ../binbuild && \
 #   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-binbuild" moonkin/meteord-ci:binbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-cibuild" ../cibuild && \
+docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-cibuild" ../cibuild && \
   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-cibuild" moonkin/meteord-ci:cibuild
