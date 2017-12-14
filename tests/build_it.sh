fed008a9d771c19e1906a0570aa28f3954980620
jens <jens.verbeken@redandivory.com>
2017-12-14 11:42:25 +0100

    change repo

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,13 +3,13 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-base" ../base && \
-#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-base" moonkin/meteord-ci:base
-# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-onbuild" ../onbuild && \
-#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-onbuild" moonkin/meteord-ci:onbuild
-# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-devbuild" ../devbuild && \
-#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-devbuild" moonkin/meteord-ci:devbuild
-# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-binbuild" ../binbuild && \
-#   docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-binbuild" moonkin/meteord-ci:binbuild
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "moonkin/meteord-ci:node-${NODE_VERSION}-cibuild" ../cibuild && \
-  docker tag "moonkin/meteord-ci:node-${NODE_VERSION}-cibuild" moonkin/meteord-ci:cibuild
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "solrapps/meteord-ci:node-${NODE_VERSION}-base" ../base && \
+#   docker tag "solrapps/meteord-ci:node-${NODE_VERSION}-base" solrapps/meteord-ci:base
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "solrapps/meteord-ci:node-${NODE_VERSION}-onbuild" ../onbuild && \
+#   docker tag "solrapps/meteord-ci:node-${NODE_VERSION}-onbuild" solrapps/meteord-ci:onbuild
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "solrapps/meteord-ci:node-${NODE_VERSION}-devbuild" ../devbuild && \
+#   docker tag "solrapps/meteord-ci:node-${NODE_VERSION}-devbuild" solrapps/meteord-ci:devbuild
+# docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "solrapps/meteord-ci:node-${NODE_VERSION}-binbuild" ../binbuild && \
+#   docker tag "solrapps/meteord-ci:node-${NODE_VERSION}-binbuild" solrapps/meteord-ci:binbuild
+docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "solrapps/meteord-ci:node-${NODE_VERSION}-cibuild" ../cibuild && \
+  docker tag "solrapps/meteord-ci:node-${NODE_VERSION}-cibuild" solrapps/meteord-ci:cibuild
